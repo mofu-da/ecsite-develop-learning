c10e9b8ffb087c9a5e9faa17fb197c8b2a9e782a
mofu-da <40336742+mofu-da@users.noreply.github.com>
2018-07-03 23:05:46 +0900

    test commit

--- src/main/java/com/example/ecsitedeveloplearning/EcsiteDevelopLearningApplication.java
+++ src/main/java/com/example/ecsitedeveloplearning/EcsiteDevelopLearningApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcsiteDevelopLearningApplication {
 
+	// Test Commit
 	public static void main(String[] args) {
 		SpringApplication.run(EcsiteDevelopLearningApplication.class, args);
 	}
